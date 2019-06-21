cee48d6eb21941d633e31fdb3ffef698309fdaf8
Corenbla <c.richard-vitton@it-students.fr>
2019-06-21 08:59:55 +0200

    Added sed lines to change the 000-default.conf

--- laravelScript.sh
+++ laravelScript.sh
@@ -52,7 +52,6 @@ wget https://github.com/vrana/adminer/releases/download/v4.7.1/adminer-4.7.1-mys
 mv adminer-4.7.1-mysql.php ./$file/adminer.php  #Installation de Adminer
 
 #####
-# TODO: sed /etc/apache2/sites-available/000-default.conf
 
   echo "
   #!bin/bash
@@ -64,7 +63,21 @@ mv adminer-4.7.1-mysql.php ./$file/adminer.php  #Installation de Adminer
   sudo sed -i '490s/Off/On/' /etc/php/7.2/apache2/php.ini
   sudo sed -i '16s/var-www/vagrant/' /etc/apache2/envvars
   sudo sed -i '17s/var-www/vagrant/' /etc/apache2/envvars
+  sed -i 's/\/var\/www\//\/var\/www\/laravel\/public/g' /etc/apache2/sites-available/000-default.conf
+  sed -i '14i\    <Directory />\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '15i\                Options FollowSymLinks\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '16i\                AllowOverride All\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '17i\    </Directory>\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '18i\    <Directory /var/www/>\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '19i\                Options Indexes FollowSymLinks MultiViews\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '20i\                AllowOverride All\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '21i\                Order allow,deny\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '22i\                allow from all\r' /etc/apache2/sites-available/000-default.conf
+  sed -i '23i\    </Directory>\r' /etc/apache2/sites-available/000-default.conf
+  sudo a2enmod rewrite
+  sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
   sudo service apache2 restart
+  echo 'Plus qu\'a créer un projet avec composer create-project --prefer-dist laravel/laravel blog'
   rm /var/www/html/install.sh
 
   " >./$file/install.sh
